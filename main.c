3ef2533ba48951d8d09875c18df3cdad0e3334ff
tukka25 <aboodytukka@gmail.com>
2024-11-09 14:30:03 +0400

    main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "ft_ping.h"
+
+int main(int ac, char **av)
+{
+	t_ping ping;
+
+	if (ac == 3)
+	{
+		setup(&ping);
+		input_parsing(av, &ping);
+		packet_send(&ping);
+		return (0);
+	}
+	print_usage();
+	return (1);
+}
